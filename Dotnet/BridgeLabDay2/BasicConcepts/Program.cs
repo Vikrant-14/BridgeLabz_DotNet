187068bdcd1ad0aaea7cea7e4bf5629b625be214
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-19 16:02:51 +0530

    [Vikrant]Refactor : Added instance variable, method and created object to access them

--- Dotnet/BridgeLabDay2/BasicConcepts/Program.cs
+++ Dotnet/BridgeLabDay2/BasicConcepts/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
 
-namespace BasicClassConcpets{
-    class BasicConcept{
-        static void Main(){
-            Console.WriteLine("OOPS CONCEPT");
+namespace BasicClassConcpets
+{
+    class BasicConcept
+    {
+        int x; // instance variable
+        string str; // instance variable of type
+
+        BasicConcept()
+        { //Parameterless Constructor
+            x = 10;
+            str = "Hello";
+        }
+
+        BasicConcept(int x, string str)
+        { //Parameterized Constructor
+            this.x = x;
+            this.str = str;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Value of x : "+ this.x);
+            Console.WriteLine("Value of str : "+ this.str);
+        }
+
+        static void Main()
+        {
+            BasicConcept b1 = new BasicConcept();
+            b1.Display();
+
+            Console.WriteLine("\n");
+
+            BasicConcept b2 = new BasicConcept(20, "Dotnet");
+            b2.Display();
         }
     }
 }
