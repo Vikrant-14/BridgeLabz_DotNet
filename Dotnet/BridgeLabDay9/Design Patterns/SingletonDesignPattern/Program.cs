79b6fa9e9437955236f2cb7c7f81ede122944daf
Vikrant-14 <vikrantgawale123@gmail.com>
2024-05-08 11:24:23 +0530

    [Vikrant] Add : Created a singleton class

---
+++ Dotnet/BridgeLabDay9/Design Patterns/SingletonDesignPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() 
+        {
+            Console.WriteLine("Singleton Instance is initialized");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if( instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Instance : " + instance.GetHashCode());
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            Singleton obj1 = Singleton.GetInstance();
+            obj1.Display();
+
+            Singleton obj2 = Singleton.GetInstance();
+            obj2.Display();
+        }
+    }
+}
