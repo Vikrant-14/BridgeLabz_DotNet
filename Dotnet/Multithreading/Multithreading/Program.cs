e63acdddae416bb503bff7578d5f7a077e1a5bcf
Vikrant-14 <vikrantgawale123@gmail.com>
2024-05-15 17:56:38 +0530

    [Vikrant] Add : Perform basic multithreading methods

--- Dotnet/Multithreading/Multithreading/Program.cs
+++ Dotnet/Multithreading/Multithreading/Program.cs
@@ -19,7 +19,45 @@ namespace MultiThreading_Examples
         {
             Console.WriteLine("Print()");
         }
-        public static void Main()
+
+        public static void Main() 
+        {
+            Console.WriteLine("Main() begins");
+
+            ThreadDemo td1 = new ThreadDemo();
+
+            Thread t1 = new Thread(td1.Show);
+            t1.Name = "thread1";
+
+            Thread t2 = new Thread(td1.Display);
+            t2.Name = "thread2";
+
+            Thread t3 = new Thread(new ThreadStart(td1.Print));
+            t3.Name = "thread3";
+
+            
+
+            t3.Priority = ThreadPriority.Highest;
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main()");
+                Thread.Sleep(1000);
+            }
+
+            
+            t3.Join(); // Main thread waits for thread3 to complete its execution
+
+            Console.WriteLine("Main() ends");
+        }
+
+
+        //Normal thread creation
+        public static void Main1()
         {
             Console.WriteLine("Main() begins");
 
