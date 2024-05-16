d83f165af338f491288ce03447aec97a0524cbe2
Vikrant-14 <vikrantgawale123@gmail.com>
2024-05-16 15:58:36 +0530

    [Vikrant] Add : Created task using Factory Property and Run()

---
+++ Dotnet/Multithreading/TaskUsingFactoryProperty/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskBasedAsynchronousProgramming
+{
+    class Program
+    {
+        public static void PrintCounter()
+        {
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+
+            for (int count = 1; count <= 5; count++)
+            {
+                Console.WriteLine($"count value: {count}");
+            }
+
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+
+            Task t1 = Task.Factory.StartNew(PrintCounter);
+
+            Task t2 = Task.Run(() => { PrintCounter();  });
+            t1.Wait(); //stops the execution of other threads util task t1 completes its execution
+            
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+    }
+}
