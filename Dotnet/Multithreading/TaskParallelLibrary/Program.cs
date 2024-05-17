ae5e0296d31b800f4b5641ddd592c6f7e0163dc4
Vikrant-14 <vikrantgawale123@gmail.com>
2024-05-17 16:30:35 +0530

    [Vikrant] Add : perform task parallel programming

---
+++ Dotnet/Multithreading/TaskParallelLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelProgrammingDemo
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Standard For loop");
+
+            int number = 10;
+
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine($"value of count = {i}, thread = {Thread.CurrentThread.ManagedThreadId}");
+                //Sleep the loop for 10 miliseconds
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Parallel For Loop");
+
+            Parallel.For(1,number+1, count =>
+            {
+                Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
+                //Sleep the loop for 10 miliseconds
+                Thread.Sleep(10);
+            });
+        }
+
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Standard For loop");
+
+            for (int i = 0; i <= 10 ; i++)
+            {
+                Console.WriteLine(i); // output is predicatable
+            }
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Parallel For Loop");
+            
+            Parallel.For(1,11, number =>  Console.WriteLine(number)); // output is unpredicatable since each task/(iteration) is performed independently on various cores
+
+        }
+    }
+}
---
+++ Dotnet/Multithreading/TaskParallelLibrary2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel_programming
+{
+    public class Program
+    {
+        static long DoSomethingLongTask()
+        {
+            long total = 0;
+
+            for (int i = 0; i < 1000000000; i++)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+
+        static void Main()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            Console.WriteLine("Parallel Foreach Loop Started");
+
+            sw.Start();
+
+            List<int> list = Enumerable.Range(1, 10).ToList();
+
+            Parallel.ForEach(list, (i) =>
+            {
+                long total = DoSomethingLongTask();
+                Console.WriteLine("{0} - {1}", i, total);
+            });
+
+
+            Console.WriteLine("Parallel Foreach Loop Ended");
+            sw.Stop();
+
+            Console.WriteLine($"Time Taken by Parallel Foreach Loop in Miliseconds {sw.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+        static void Main1(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            Console.WriteLine("Standard Foreach Loop Started");
+
+            sw.Start();
+
+            List<int> list = Enumerable.Range(1,10).ToList();
+
+            foreach (var i in list)
+            {
+                long total = DoSomethingLongTask();
+                Console.WriteLine("{0} - {1}", i, total);
+            }
+
+
+            Console.WriteLine("Standard Foreach Loop Ended");
+            sw.Stop();
+
+            Console.WriteLine($"Time Taken by Standard Foreach Loop in Miliseconds {sw.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+    }
+}
