0d399c98202bdc6f46015208db987243448cbbfb
Vikrant-14 <vikrantgawale123@gmail.com>
2024-05-24 16:20:07 +0530

    [Vikrant] Add : checked how various types of parameters can use in stored procedures

--- Dotnet/SQL/STORED PROCEDURE.sql
+++ Dotnet/SQL/STORED PROCEDURE.sql
@@ -56,3 +56,105 @@ END;
 -- Deleting a Stored Procedure :
 ---------------------------------
 DROP PROCEDURE customer_list;
+
+---------------------------------------------------
+--Creating a stored procedure with one parameter :
+---------------------------------------------------
+CREATE PROCEDURE find_customer(@name AS varchar(255))
+AS
+BEGIN
+	SELECT 
+		first_name + ' ' + last_name Full_Name,
+		email Email,
+		phone Phone,
+		city City
+	From
+		customers
+	Where
+		first_name = @name
+	Order BY
+		first_name,
+		last_name;
+END
+
+Execute find_customer 'Alice';
+
+--------------------------------------------------------
+--Creating a stored procedure with multiple parameters :
+--------------------------------------------------------
+CREATE PROCEDURE Search_Customer(@firstname AS varchar(255), @lastname as varchar(255))
+AS
+BEGIN
+	SELECT 
+		first_name + ' ' + last_name Full_Name,
+		email Email,
+		phone Phone,
+		city City
+	From
+		customers
+	Where
+		first_name = @firstname OR
+		last_name = @lastname
+	Order BY
+		first_name,
+		last_name;
+END
+
+Execute Search_Customer 'Alice', 'Lewis' ;
+
+--Using Named Parameters :
+---------------------------
+Execute Search_Customer
+	@firstname = 'Alice',
+	@lastname = 'Lee';
+
+------------------------------
+--Creating text parameters : 
+------------------------------
+CREATE PROCEDURE Search_Customer_Based_on_City(@firstname AS varchar(max), @lastname as varchar(max), @cityname as varchar(max))
+AS
+BEGIN
+	SELECT 
+		first_name + ' ' + last_name Full_Name,
+		email Email,
+		phone Phone,
+		city City
+	From
+		customers
+	Where
+		first_name = @firstname OR
+		last_name = @lastname OR
+		city LIKE '%' + @cityname + '%' 
+	Order BY
+		first_name,
+		last_name;
+END
+
+EXECUTE Search_Customer_Based_on_City
+	@firstname = 'John',
+	@lastname = 'Lewis',
+	@cityname = 'Some';
+
+-----------------------------
+--Creating Optional Parameter : 
+-----------------------------
+CREATE PROCEDURE find_products(@min_price as Decimal = 0, @max_price as Decimal = 99999, @name as varchar(max))
+AS
+BEGIN
+	SELECT
+		Name,
+		Price
+	FROM
+		Product
+	WHERE
+		Price >= @min_price AND
+		Price <= @max_price AND
+		Name LIKE '%' + @name +'%'
+	ORDER BY
+		Price;
+END
+
+EXECUTE find_products  @name = 'c';
+
+Select * from product;
+	
